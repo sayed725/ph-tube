4684c992f59b6950d9132d111bb3ce100bea24b9
sayed725 <ssayed72533@gmail.com>
2024-10-04 15:57:52 +0600

    added serch function

--- video.js
+++ video.js
@@ -9,9 +9,9 @@ function getTimeString(time) {
   return `${hour} hour  ${minute} minute ${remainingSecond} second ago`;
 }
 
-const removeActiveClass = ()=>{
+const removeActiveClass = () => {
   const buttons = document.getElementsByClassName('category-btn')
-  for(let button of buttons){
+  for (let button of buttons) {
     button.classList.remove('active')
   }
 }
@@ -22,12 +22,16 @@ const loadCategories = () => {
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.log(error))
 };
-const loadVideos = () => {
-  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res => res.json())
+const loadVideos = (searchText = "") => {
+  //fetch the data
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+  )
+    .then((res) => res.json())
     .then((data) => displayVideos(data.videos))
-    .catch((error) => console.log(error))
+    .catch((error) => console.log(error));
 };
+
 
 const loadCategoryVideos = (id) => {
   // alert(id);
@@ -43,6 +47,26 @@ const loadCategoryVideos = (id) => {
     })
     .catch((error) => console.log(error))
 
+}
+
+const loadDetails= async (videoId)=>{
+  const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  const res = await fetch(uri)
+  const data = await res.json()
+  displayDetails(data.video)
+}
+const displayDetails = (video) =>{
+  const detailContainer = document.getElementById("modal-content");
+
+  detailContainer.innerHTML = `
+   <img src=${video.thumbnail} />
+   <p>${video.description}</p>
+  `;
+
+  // way-1
+  // document.getElementById("showModalData").click();
+  //way-2
+  document.getElementById("customModal").showModal();
 }
 
 // const cardDemo = {
@@ -118,7 +142,9 @@ const displayVideos = (videos) => {
 
             
         </div>
-  <p> </P
+  <p> <button  onclick="loadDetails('${
+          video.video_id
+        }')" class="btn btn-sm btn-error">details</button> </P>
   </div>
    
   </div>
@@ -145,6 +171,10 @@ const displayCategories = (categories) => {
     categoryContainer.append(buttonContainer);
   });
 }
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  loadVideos(e.target.value);
+});
 
 loadCategories()
 loadVideos()
